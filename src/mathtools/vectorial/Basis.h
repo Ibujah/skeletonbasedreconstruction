aa1231950ff65fb4ebeda5b930a413711465ddc0
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-01-18 10:03:36 +0100

    Added class representing basis of a vectorial space

---
+++ src/mathtools/vectorial/Basis.h
@@ -0,0 +1,32 @@
+#ifndef _BASIS_H_
+#define _BASIS_H_
+
+/**
+ *  \file  Basis.h
+ *  \brief Defines a basis of a vectorial space
+ *  \author Bastien Durix
+ */
+
+/**
+ *  \brief Mathematical tools
+ */
+namespace mathtools
+{
+	/**
+	 *  \brief Vectorial tools
+	 */
+	namespace vectorial
+	{
+		/**
+		 *  \class Basis
+		 *  \brief Basis of a vectorial space
+		 */
+		template<unsigned int Dim>
+		class Basis
+		{
+			
+		}
+	}
+}
+
+#endif //_BASIS_H_
