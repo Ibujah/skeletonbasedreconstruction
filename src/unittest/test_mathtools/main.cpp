f63da55d26fb685320bf6a4eec17d49d19866fd6
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-02-02 17:16:28 +0100

    Added test for compositor

--- src/unittest/test_mathtools/main.cpp
+++ src/unittest/test_mathtools/main.cpp
@@ -27,13 +27,15 @@ SOFTWARE.
  */
 
 #include <iostream>
-#include <mathtools/application/Bspline.h>
 #include <math.h>
+#include <mathtools/application/Bspline.h>
+#include <mathtools/application/Compositor.h>
+#include <mathtools/application/LinearApp.h>
 
-int main()
+using namespace mathtools::application;
+
+unsigned int bsplinetest()
 {
-	unsigned int return_value = 0;
-
 	bool bspline_test = true;
 	unsigned int fraction = 100;
 	
@@ -61,7 +63,7 @@ int main()
 			{
 				double t = (double)i*(1.0/(double)fraction);
 				
-				double res = mathtools::application::BsplineBasis(t,degree,indice,node);
+				double res = BsplineBasis(t,degree,indice,node);
 				
 				double ref = pow((1-t),degree-indice)*pow(t,indice)*(double)bin_coeff[indice];
 				
@@ -70,6 +72,8 @@ int main()
 			}
 		}
 	}
+
+	unsigned int return_value = 0;
 	if(!bspline_test)
 	{
 		return_value = -1;
@@ -78,5 +82,64 @@ int main()
 	else
 		std::cout << "Success!" << std::endl;
 	
+	return return_value;
+}
+
+unsigned int compositortest()
+{
+	std::cout << "Compositor test... ";
+	
+	Eigen::Matrix3d mat_lin;
+	mat_lin <<  0.0, 2.0, 0.0,
+			   -1.0, 0.0, 0.0,
+			    0.0, 0.0, 3.0;
+	unsigned int degree = 3;
+	unsigned int nbctrlpt = 4;
+	Eigen::Matrix<double,1,Eigen::Dynamic> nodevec(1,nbctrlpt + degree - 1);
+	nodevec << 0.0, 0.0, 0.0, 1.0, 1.0, 1.0;
+	Eigen::Matrix<double,3,Eigen::Dynamic> ctrlpt(3,nbctrlpt);
+	ctrlpt << 0.0, 1.0, 1.0, 0.0,
+			  0.0, 0.0, 1.0, 1.0,
+			  0.0, 0.0, 0.0, 0.0;
+	Bspline<3> bsp(ctrlpt,nodevec,degree);
+	Compositor<LinearApp<3,3>,Bspline<3> > comp(LinearApp<3,3>(mat_lin),bsp);
+	
+	bool compositor_test = true;
+	for(unsigned int i = 0; i < 101; i++)
+	{
+		double t = (double)i*0.01;
+
+		Eigen::Vector3d vecres = comp(t);
+
+		Eigen::Vector3d vecref = mat_lin * bsp(t);
+		
+		if( (vecres-vecref).norm() > std::numeric_limits<double>::epsilon() )
+		{
+			compositor_test = false;
+		}
+	}
+
+	unsigned int return_value = 0;
+	if(!compositor_test)
+	{
+		return_value = -1;
+		std::cout << "Fail!" << std::endl;
+	}
+	else
+		std::cout << "Success!" << std::endl;
+	
+	return return_value;
+}
+
+int main()
+{
+	unsigned int return_value = 0;
+	
+	unsigned int bspline_test_value = bsplinetest();
+	if(bspline_test_value != 0) return_value = -1;
+	
+	unsigned int compositor_test_value = compositortest();
+	if(compositor_test_value != 0) return_value = -1;
+
 	return return_value;
 }
