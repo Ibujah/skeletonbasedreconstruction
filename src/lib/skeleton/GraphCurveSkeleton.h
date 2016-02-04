821551ba3c3539709b350481405d79a8f2b2e63d
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-02-04 12:34:24 +0100

    Started GraphCurveSkeleton class. Beautifulgit add GraphCurveSkeleton.h

--- src/lib/skeleton/GraphCurveSkeleton.h
+++ src/lib/skeleton/GraphCurveSkeleton.h
@@ -29,6 +29,7 @@ SOFTWARE.
 #ifndef _GRAPHCURVESKELETON_H_
 #define _GRAPHCURVESKELETON_H_
 
+#include <memory>
 #include <map>
 #include <Eigen/Dense>
 #include <boost/graph/adjacency_list.hpp>
@@ -50,15 +51,91 @@ namespace skeleton
 	{
 		protected:
 			/**
-			 *  \brief All nodes in the graph curve skeleton
+			 *  \brief Graph curve skeleton shared pointer
 			 */
-			std::map<unsigned int,Eigen::Matrix<double,model::meta<Model>::stordim,1> > m_node;
+			using Ptr = std::shared_ptr<GraphCurveSkeleton<Model> >;
+			
+			/**
+			 *  \brief Storage type of the objects in the skeleton
+			 */
+			using Stor = Eigen::Matrix<double,model::meta<Model>::stordim,1>;
+
+			/**
+			 *  \brief Model used to give a meaning to the skeleton
+			 *
+			 *  \details This class will not be modified inside the skeleton:
+			 *			 once it is created, it should not be changed
+			 */
+			typename Model::Ptr m_model;
+			
+			/**
+			 *  \brief All nodes in the graph curve skeleton
+			 *
+			 *  \details This storage will be intrepreted by the model
+			 */
+			std::map<unsigned int,Stor> m_node;
 			
 			/**
 			 *  \brief Graph of the curve skeleton
 			 */
-			boost::adjacency_list<> m_graph;
+			boost::adjacency_list<boost::listS,boost::listS,boost::undirectedS> m_graph;
+			
+		public:
+			/**
+			 *  \brief Constructor
+			 *
+			 *  \param model initialisation of the model to use
+			 */
+			GraphCurveSkeleton(const typename Model::Ptr model) : m_model(model) {}
 
+			/**
+			 *  \brief Constructor
+			 *
+			 *  \param model initialisation of the model to use
+			 */
+			GraphCurveSkeleton(const Model &model) : GraphCurveSkeleton(new typename Model::Ptr(model)) {}
+
+			/**
+			 *  \brief Copy constructor
+			 *
+			 *  \param grsk skeleton to copy
+			 */
+			GraphCurveSkeleton(const GraphCurveSkeleton<Model> &grsk) :
+				GraphCurveSkeleton(grsk.m_model), m_node(grsk.m_node), m_graph(grsk.m_graph) {}
+
+			/**
+			 *  \brief Adding node function
+			 *
+			 *  \param node node to add
+			 *
+			 *  \return node indice
+			 *
+			 *  \details The function does not check if the node is already in the skeleton or not
+			 */
+			unsigned int addNode(const Stor &node)
+			{
+				unsigned int key = *(m_node.rbegin())+1;
+				m_node.insert(std::pair<unsigned int,Stor>(key,node));
+				return key;
+			}
+
+			/**
+			 *  \brief Adding node function
+			 *
+			 *  \tparam TypeNode node type to add
+			 *
+			 *  \param node node to add
+			 *
+			 *  \return node indice
+			 *
+			 *  \details The function does not check if the node is already in the skeleton or not
+			 */
+			template<typename TypeNode>
+			unsigned int addNode(const TypeNode &node)
+			{
+				Stor vec = m_model->template toVec<TypeNode>(node);
+				return addNode<Eigen::Matrix<double,model::meta<Model>::stordim,1> >(vec);
+			}
 	};
 }
 
