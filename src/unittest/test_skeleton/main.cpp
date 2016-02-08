8a7c0ec59ecb1ad54ef598693a416c136a1b6df7
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-02-08 15:53:01 +0100

    Separated unit tests in different cases

--- src/unittest/test_skeleton/main.cpp
+++ src/unittest/test_skeleton/main.cpp
@@ -42,86 +42,110 @@ using namespace mathtools::geometry::euclidian;
 #define BOOST_TEST_MODULE TestMathtools
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( SkeletonTest )
+skeleton::GraphCurveSkeleton<skeleton::model::Classic<2> >::Ptr skel = skeleton::GraphCurveSkeleton<skeleton::model::Classic<2> >::Ptr();
+Frame<2>::Ptr frame = Frame<2>::Ptr();
+skeleton::model::Classic<2>::Ptr modclass = skeleton::model::Classic<2>::Ptr();
+unsigned int ind1 = 0;
+unsigned int ind2 = 0;
+
+BOOST_AUTO_TEST_CASE( SkeletonCreation )
 {
 	// frame creation
-	Frame<2>::Ptr frame = Frame<2>::CreateFrame(Eigen::Vector2d(1.0,0.0),Eigen::Vector2d(2.0,1.0),Eigen::Vector2d(1.0,0.0));
+	frame = Frame<2>::CreateFrame(Eigen::Vector2d(1.0,0.0),Eigen::Vector2d(2.0,1.0),Eigen::Vector2d(1.0,0.0));
 
 	// model creation
-	skeleton::model::Classic<2>::Ptr modclass(new skeleton::model::Classic<2>{frame});
+	modclass = skeleton::model::Classic<2>::Ptr(new skeleton::model::Classic<2>{frame});
 	
+	// skeleton creation
+	skel = skeleton::GraphCurveSkeleton<skeleton::model::Classic<2> >::Ptr(new skeleton::GraphCurveSkeleton<skeleton::model::Classic<2> >(modclass));
+}
+
+BOOST_AUTO_TEST_CASE( AddingNode )
+{
 	// creating a random node
 	Eigen::Vector3d vec3((double)(rand()%201)/10.0-1.0,
 						 (double)(rand()%201)/10.0-1.0,
 						 (double)(rand()%201)/10.0);
 
-	// skeleton creation
-	skeleton::GraphCurveSkeleton<skeleton::model::Classic<2> > skel(modclass);
-	
 	// adding a node
-	unsigned int ind = skel.addNode(vec3);
+	ind1 = skel->addNode(vec3);
 	
 	// getting the node (vector form)
-	Eigen::Vector3d vec3_2 = skel.getNode(ind);
+	Eigen::Vector3d vec = skel->getNode(ind1);
 	
-	BOOST_CHECK( vec3_2.isApprox(vec3,std::numeric_limits<double>::epsilon()) );
+	BOOST_REQUIRE( vec.isApprox(vec3,std::numeric_limits<double>::epsilon()) );
 	
 	// getting the node (center form)
-	Point<2> pt = skel.getNode<Point<2> >(ind);
+	Point<2> pt = skel->getNode<Point<2> >(ind1);
 	
-	BOOST_CHECK( pt.getCoords().isApprox(frame->getBasis()->getMatrix()*vec3.block<2,1>(0,0)+frame->getOrigin()) );
+	BOOST_REQUIRE( pt.getCoords().isApprox(frame->getBasis()->getMatrix()*vec3.block<2,1>(0,0)+frame->getOrigin()) );
+}
 
+BOOST_AUTO_TEST_CASE( AddingSphere )
+{
 	// hypersphere creation
 	HyperSphere<2> sph(Point<2>(1.0,0.0),5.0,frame);
 
 	// adding a node
-	unsigned int ind2 = skel.addNode<HyperSphere<2> >(sph);
+	ind2 = skel->addNode<HyperSphere<2> >(sph);
 	
 	// get the vector associated to the node
-	Eigen::Vector3d vecsph = skel.getNode(ind2);
+	Eigen::Vector3d vecsph = skel->getNode(ind2);
 	
 	// convert the node to a sphere
-	HyperSphere<2> sphcpy = skel.getModel()->toObj<HyperSphere<2> >(vecsph);
+	HyperSphere<2> sphcpy = skel->getModel()->toObj<HyperSphere<2> >(vecsph);
 	
-	BOOST_CHECK( sphcpy.getCenter().getCoords().isApprox(sph.getCenter().getCoords(),std::numeric_limits<double>::epsilon()) );
-	BOOST_CHECK( sphcpy.getRadius() == sph.getRadius() );
-	BOOST_CHECK( sphcpy.getFrame()->getBasis()->getMatrix().isApprox(sph.getFrame()->getBasis()->getMatrix(),std::numeric_limits<double>::epsilon()) );
-	BOOST_CHECK( ind != ind2);
-	
+	BOOST_REQUIRE( sphcpy.getCenter().getCoords().isApprox(sph.getCenter().getCoords(),std::numeric_limits<double>::epsilon()) );
+	BOOST_REQUIRE( sphcpy.getRadius() == sph.getRadius() );
+	BOOST_REQUIRE( sphcpy.getFrame()->getBasis()->getMatrix().isApprox(sph.getFrame()->getBasis()->getMatrix(),std::numeric_limits<double>::epsilon()) );
+	BOOST_REQUIRE( ind1 != ind2);
+}
+
+BOOST_AUTO_TEST_CASE( getAllNodes )
+{
 	// get all indices
 	std::list<unsigned int> listnod;
 
-	skel.getAllNodes(listnod);
+	skel->getAllNodes(listnod);
 
 	listnod.unique();
 
-	BOOST_CHECK( listnod.size() == 2 );
-	BOOST_CHECK( std::find(listnod.begin(),listnod.end(),ind) != listnod.end() );
-	BOOST_CHECK( std::find(listnod.begin(),listnod.end(),ind2) != listnod.end() );
-	
+	BOOST_REQUIRE( listnod.size() == 2 );
+	BOOST_REQUIRE( std::find(listnod.begin(),listnod.end(),ind1) != listnod.end() );
+	BOOST_REQUIRE( std::find(listnod.begin(),listnod.end(),ind2) != listnod.end() );
+}
+
+BOOST_AUTO_TEST_CASE( addEdge )
+{
 	// adding an edge
-	skel.addEdge(ind, ind2);
+	skel->addEdge(ind1, ind2);
 
 	std::list<unsigned int> neigh;
-	skel.getNeighbors(ind,neigh);
+	skel->getNeighbors(ind1,neigh);
 	
-	BOOST_CHECK( neigh.size() == 1 );
-	BOOST_CHECK( *(neigh.begin()) == ind2 );
+	BOOST_REQUIRE( neigh.size() == 1 );
+	BOOST_REQUIRE( *(neigh.begin()) == ind2 );
+}
 
+BOOST_AUTO_TEST_CASE( remEdge )
+{
 	// removing an edge
-	skel.remEdge(ind, ind2);
+	skel->remEdge(ind1, ind2);
 	
-	neigh.erase(neigh.begin(),neigh.end());
-	skel.getNeighbors(ind,neigh);
+	std::list<unsigned int> neigh;
+	skel->getNeighbors(ind1,neigh);
 	
-	BOOST_CHECK( neigh.size() == 0 );
+	BOOST_REQUIRE( neigh.size() == 0 );
+}
 
-	skel.remNode(ind);
+BOOST_AUTO_TEST_CASE( remNode )
+{
+	skel->remNode(ind1);
 
 	try
 	{
-		skel.getNode(ind);
-		BOOST_ERROR( "Deleted a node already removed" );
+		skel->getNode(ind1);
+		BOOST_FAIL( "Deleted a node already removed" );
 	}
 	catch(...)
 	{}
