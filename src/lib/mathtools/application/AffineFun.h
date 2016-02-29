cddbaa11a54cb6c0da86b45ec89f2f0eb8544d67
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-02-29 18:19:02 +0100

    Added affine function

---
+++ src/lib/mathtools/application/AffineFun.h
@@ -0,0 +1,114 @@
+/*
+Copyright (c) 2016 Bastien Durix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
+ *  \file AffineFun.h
+ *  \brief Defines affine function
+ *  \author Bastien Durix
+ */
+
+#ifndef _AFFINEFUNCTION_H_
+#define _AFFINEFUNCTION_H_
+
+#include "Application.h"
+#include <Eigen/Dense>
+
+/**
+ *  \brief Mathematical tools
+ */
+namespace mathtools
+{
+	/**
+	 *  \brief Application tools
+	 */
+	namespace application
+	{
+		/**
+		 * \brief Affine function
+		 */
+		class AffineFun : public Application<double,double>
+		{
+			public:
+				/**
+				 *  \brief Out type of result funtion
+				 */
+				using outType = double;
+				/**
+				 *  \brief In type of result funtion
+				 */
+				using inType = double;
+
+			protected:
+				/**
+				 *  \brief slope
+				 */
+				double m_a;
+
+				/**
+				 *  \brief y-intercept
+				 */
+				double m_b;
+
+			public:
+				/**
+				 *  \brief Construtor
+				 *
+				 *  \brief a slope
+				 *  \brief b y-intercept
+				 */
+				AffineFun(double a, double b) : m_a(a), m_b(b)
+				{}
+
+				/**
+				 *  \brief Copy construtor
+				 *
+				 *  \param afffun LinearApplication to copy in
+				 */
+				AffineFun(const AffineFun &afffun) : m_a(afffun.m_a), m_b(afffun.m_b)
+				{}
+
+				/**
+				 *  \brief Application call
+				 *
+				 *  \param t application input
+				 * 
+				 *  \return application output 
+				 */
+				virtual double operator()(const double &t) const
+				{
+					return m_a * t;
+				}
+
+				/**
+				 *  \brief Jacobian call
+				 *
+				 *  \return Jacobian evaluation at vec
+				 */
+				virtual double jac(const double &) const
+				{
+					return m_a;
+				}
+		};
+	}
+}
+
+#endif //_AFFINEFUN_H_
