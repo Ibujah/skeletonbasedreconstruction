b97f925d4a56e0c67da3cf7e147ead8884e75258
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-03-01 15:04:42 +0100

    Added modifying methods in ComposedCurveSkeleton

--- src/lib/skeleton/ComposedCurveSkeleton.h
+++ src/lib/skeleton/ComposedCurveSkeleton.h
@@ -54,6 +54,17 @@ namespace skeleton
 
 		protected:
 			/**
+			 *  \brief Vertex property
+			 */
+			struct VertexProperty 
+			{
+				/**
+				 *  \brief Vertex index
+				 */
+				unsigned int index;
+			};
+			
+			/**
 			 *  \brief Edge property
 			 */
 			struct EdgeProperty 
@@ -74,13 +85,117 @@ namespace skeleton
 			 *
 			 *  \details Creates a undirected graph with vertices using VertexProperty
 			 */
-			using GraphType = boost::adjacency_list<boost::listS,boost::listS,boost::directedS,boost::no_property>;
+			using GraphType = boost::adjacency_list<boost::listS,boost::listS,boost::directedS,VertexProperty,EdgeProperty>;
 
 			/**
 			 *  \brief Graph of the composed skeleton
 			 */
 			GraphType m_graph;
+			
+			/**
+			 *  \brief Last added node, to compute the key of the next added node
+			 */
+			unsigned int m_nodelast;
+			
+			/**
+			 *  \brief Last added edge, to compute the key of the next added edge
+			 */
+			unsigned int m_edgelast;
 
+		public:
+			/**
+			 *  \brief Constructor
+			 */
+			ComposedCurveSkeleton() : m_graph(), m_nodelast(0), m_edgelast(0) {}
+		
+		protected:
+			/**
+			 *  \brief Get the vertex descriptor associated to a vertex index
+			 *
+			 *  \param index  vertex index
+			 *  \param v_desc out vertex descriptor
+			 *
+			 *  \return false if the index is not in the skeleton
+			 */
+			bool getDesc(unsigned int index, typename boost::graph_traits<GraphType>::vertex_descriptor &v_desc) const
+			{
+				typename boost::graph_traits<GraphType>::vertex_iterator vi, vi_end;
+				bool v_found = false;
+				for(boost::tie(vi,vi_end) = boost::vertices(m_graph); vi != vi_end && !v_found; vi++)
+				{
+					if(m_graph[*vi].index == index)
+					{
+						v_desc = *vi;
+						v_found = true;
+					}
+				}
+				
+				return v_found;
+			}
+
+		public://modifying functions
+			/**
+			 *  \brief Adding node function
+			 *
+			 *  \return node index
+			 */
+			unsigned int addNode()
+			{
+				unsigned int index = m_nodelast++;
+				//Adds the vertex in the graph, with index and storage information
+				typename boost::graph_traits<GraphType>::vertex_descriptor v_desc = boost::add_vertex(m_graph);
+				m_graph[v_desc].index = index;
+				return index;
+			}
+
+			/**
+			 *  \brief Adding node function, with given index
+			 *
+			 *  \param index new node index
+			 *
+			 *  \return node index
+			 */
+			unsigned int addNode(unsigned int index)
+			{
+				typename boost::graph_traits<GraphType>::vertex_descriptor v_desc;
+				if(!getDesc(index,v_desc))
+				{
+					//Adds the vertex in the graph, with index and storage information
+					typename boost::graph_traits<GraphType>::vertex_descriptor v_desc = boost::add_vertex(m_graph);
+					m_graph[v_desc].index = index;
+				}
+				return index;
+			}
+
+			/**
+			 *  \brief Adding edge function
+			 *
+			 *  \param ind1   first node index
+			 *  \param ind2   second node index
+			 *  \param branch branch to add
+			 *
+			 *  \return false if one of the two nodes is not in the skeleton
+			 */
+			bool addEdge(unsigned int ind1, unsigned int ind2, const BranchType &branch)
+			{
+				bool v_found = false;
+				if(ind1 != ind2)
+				{
+					// first step, get the descriptors corresponding to indices
+					typename boost::graph_traits<GraphType>::vertex_descriptor v_desc1, v_desc2;
+					v_found = getDesc(ind1,ind2,v_desc1,v_desc2);
+
+					// second step, add the edge
+					if(v_found)
+					{
+						typename boost::graph_traits<GraphType>::edge_descriptor e_desc;
+						e_desc = boost::add_edge(v_desc1,v_desc2,m_graph);
+						m_graph[e_desc].index = m_edgelast++;
+						m_graph[e_desc].branch = BranchType::Ptr(new BranchType(branch));
+					}
+				}
+				return v_found;
+			}
 	};
 }
 
