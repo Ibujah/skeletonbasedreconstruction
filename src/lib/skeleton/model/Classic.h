e0a566622a2dbc1a86ecaa2e0cc91fa32b389a8d
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-03-03 14:04:08 +0100

    Mistake correction

--- src/lib/skeleton/model/Classic.h
+++ src/lib/skeleton/model/Classic.h
@@ -77,7 +77,7 @@ namespace skeleton
 				 *
 				 *  \param frame frame of the skeleton
 				 */
-				Classic(const typename mathtools::affine::Frame<Dim>::Ptr frame = mathtools::affine::Frame<2>::CanonicFrame()) : m_frame(frame) {}
+				Classic(const typename mathtools::affine::Frame<Dim>::Ptr frame = mathtools::affine::Frame<Dim>::CanonicFrame()) : m_frame(frame) {}
 				
 				/**
 				 *  \brief Copy constructor
