eccecda936c30ae9aecb8b1692f1913ef5913869
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-03-27 15:53:10 +0200

    Added user input function to click nodes on skeleton

---
+++ src/utils/userinput/ClickSkelNode.cpp
@@ -0,0 +1,111 @@
+/*
+Copyright (c) 2016 Bastien Durix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+/**
+ *  \file ClickSkelNode.cpp
+ *  \brief Allows user to click nodes on skeleton
+ *  \author Bastien Durix
+ */
+
+#include "ClickSkelNode.h"
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+struct DatImg
+{
+	std::map<unsigned int, Eigen::Vector2d> m_nod;
+	std::vector<unsigned int> clicknod;
+	cv::Mat &img;
+	
+	DatImg(cv::Mat &im) : img(im){};
+};
+
+void extclickonMouse( int event, int xc, int yc, int, void* data )
+{
+	DatImg *datim = (DatImg*)data;
+    if( event == cv::EVENT_LBUTTONDOWN )
+    {
+		Eigen::Vector2d pt(xc,yc);
+		unsigned int ind = 0;
+		Eigen::Vector2d nodcoords;
+		double dist_min=-1.0;
+		for(std::map<unsigned int,Eigen::Vector2d>::iterator it = datim->m_nod.begin(); it != datim->m_nod.end(); it++)
+		{
+			double dist = (pt-it->second).squaredNorm();
+			if(dist_min == -1.0 || dist < dist_min)
+			{
+				dist_min = dist;
+				ind=it->first;
+				nodcoords = it->second;
+			}
+		}
+		datim->clicknod.push_back(ind);
+
+		cv::circle(datim->img,cv::Point(nodcoords.x(),nodcoords.y()),2,cv::Scalar(0,255,0),-1);
+	}
+}
+
+template<typename Model>
+std::vector<unsigned int> ClickSkelNodes_helper(cv::Mat &img, const typename skeleton::GraphCurveSkeleton<Model>::Ptr grskel, unsigned int nbnod, const mathtools::affine::Frame<2>::Ptr frame)
+{
+	std::list<unsigned int> l_allnodes;
+	grskel->getAllNodes(l_allnodes);
+	
+	DatImg datim(img);
+	
+	for(std::list<unsigned int>::iterator it = l_allnodes.begin(); it != l_allnodes.end(); it++)
+	{
+		if(grskel->getNodeDegree(*it) != 2)
+			datim.m_nod.insert(std::pair<unsigned int,Eigen::Vector2d>(*it,grskel->template getNode<mathtools::affine::Point<2> >(*it).getCoords(frame)));
+	}
+	
+	
+	datim.clicknod.reserve(nbnod);
+	datim.clicknod.resize(0);
+	
+	cv::namedWindow("Click", CV_WINDOW_AUTOSIZE);
+    cv::setMouseCallback("Click",extclickonMouse,&datim);
+    cv::imshow("Click",img);
+	
+	while(datim.clicknod.size() != nbnod)
+	{
+		cv::imshow("Click",img);
+		cv::waitKey(10);
+	}
+
+	cv::setMouseCallback("Click",NULL);
+	cv::destroyWindow("Click");
+
+	return datim.clicknod;
+}
+
+std::vector<unsigned int> userinput::ClickSkelNodes(cv::Mat &img, const skeleton::GraphSkel2d::Ptr grskel, unsigned int nbnod, const mathtools::affine::Frame<2>::Ptr frame)
+{
+	return ClickSkelNodes_helper<skeleton::model::Classic<2> >(img,grskel,nbnod,frame);
+}
+
+std::vector<unsigned int> userinput::ClickSkelNodes(cv::Mat &img, const skeleton::GraphProjSkel::Ptr grskel, unsigned int nbnod, const mathtools::affine::Frame<2>::Ptr frame)
+{
+	return ClickSkelNodes_helper<skeleton::model::Projective>(img,grskel,nbnod,frame);
+}
---
+++ src/utils/userinput/ClickSkelNode.h
@@ -0,0 +1,69 @@
+/*
+Copyright (c) 2016 Bastien Durix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+/**
+ *  \file ClickSkelNode.h
+ *  \brief Allows user to click nodes on skeleton
+ *  \author Bastien Durix
+ */
+
+#ifndef _CLICKSKELNODE_H_
+#define _CLICKSKELNODE_H_
+
+#include <opencv2/core/core.hpp>
+
+#include <skeleton/Skeletons.h>
+#include <mathtools/affine/Frame.h>
+#include <mathtools/affine/Point.h>
+
+/**
+ *  \brief User input functions
+ */
+namespace userinput
+{
+	/**
+	 *  \brief Click skeleton nodes on 2d graph skeleton
+	 *
+	 *  \param img     input image displaying the skeleton/output image displaying the clickes nodes
+	 *  \param grskel  2d graph skeleton
+	 *  \param nbnod   number of nodes to click
+	 *  \param frame   frame of the image
+	 *
+	 *  \return id of clicked nodes
+	 */
+	std::vector<unsigned int> ClickSkelNodes(cv::Mat &img, const skeleton::GraphSkel2d::Ptr grskel, unsigned int nbnod, const mathtools::affine::Frame<2>::Ptr frame);
+
+	/**
+	 *  \brief Click skeleton nodes on projective graph skeleton
+	 *
+	 *  \param img     input image displaying the skeleton/output image displaying the clickes nodes
+	 *  \param grskel  projective graph skeleton
+	 *  \param nbnod   number of nodes to click
+	 *  \param frame   frame of the image
+	 *
+	 *  \return id of clicked nodes
+	 */
+	std::vector<unsigned int> ClickSkelNodes(cv::Mat &img, const skeleton::GraphProjSkel::Ptr grskel, unsigned int nbnod, const mathtools::affine::Frame<2>::Ptr frame);
+}
+
+#endif //_CLICKSKELNODE_H_
