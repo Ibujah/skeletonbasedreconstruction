eee870bcbbc6eb72de7fbe1e7843ff34017618f4
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-04-29 17:37:03 +0200

    Rectified ellipse computation in perspective skeleton

--- src/lib/skeleton/model/Perspective.cpp
+++ src/lib/skeleton/model/Perspective.cpp
@@ -80,20 +80,22 @@ mathtools::geometry::euclidian::HyperEllipse<2> skeleton::model::Perspective::to
 		const Eigen::Matrix<double,skeleton::model::meta<skeleton::model::Projective>::stordim,1> &vec,
 		const mathtools::geometry::euclidian::HyperEllipse<2> &) const
 {
-	double z = 1.0/(2.0*sqrt(vec(0)*vec(0) + vec(1)*vec(1) - vec(2)*vec(2) - 1.0));
+	double z = 1.0/(sqrt(vec(0)*vec(0) + vec(1)*vec(1) + 1.0 - vec(2)*vec(2)));
 	double x = vec(0) * z;
 	double y = vec(1) * z;
-
-	mathtools::affine::Point<2> pt((-x*z)/(2.0-z*z),(-y*z)/(2.0-z*z),m_frame2);
 	
 	Eigen::Matrix2d mat;
-	mat << (1.0-x*x)/(2.0-z*z) , (-x*y)/(2.0-z*z),
-		   (-x*y)/(2.0-z*z)     , (1.0-y*y)/(2.0-z*z);
+	mat << 1.0-x*x , -x*y    ,
+		   -x*y    , 1.0-y*y ;
 	
 	Eigen::SelfAdjointEigenSolver<Eigen::Matrix2d> solv(mat);
-	Eigen::Matrix2d axes = m_frame2->getBasis()->getMatrix()*solv.operatorSqrt()*m_frame2->getBasis()->getMatrixInverse();
 	
-	return mathtools::geometry::euclidian::HyperEllipse<2>(pt,axes);
+	Eigen::Matrix2d axes = Eigen::Matrix2d::Identity();
+	double fac = Eigen::Vector2d(-x*z,-y*z).transpose()*mat.inverse()*Eigen::Vector2d(-x*z,-y*z) - 1.0 + z*z;
+	axes = solv.eigenvectors()*Eigen::DiagonalMatrix<double,2>(Eigen::Vector2d(sqrt(fac/solv.eigenvalues()(0)),sqrt(fac/solv.eigenvalues()(1))));
+	
+	mathtools::affine::Point<2> pt(vec(0),vec(1),m_frame2);
+	return mathtools::geometry::euclidian::HyperEllipse<2>(pt,m_frame2->getBasis()->getMatrix()*axes);
 }
 
 mathtools::geometry::euclidian::Line<4> skeleton::model::Perspective::toObj(
