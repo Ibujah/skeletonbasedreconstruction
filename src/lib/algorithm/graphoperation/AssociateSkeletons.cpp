58d6c9ee6560e7f4b5e37156c123c1ed82f32e62
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-05-19 09:27:55 +0200

    Added edge encoder and decoder to topology recovering

--- src/lib/algorithm/graphoperation/AssociateSkeletons.cpp
+++ src/lib/algorithm/graphoperation/AssociateSkeletons.cpp
@@ -29,6 +29,7 @@ SOFTWARE.
 
 #include "AssociateSkeletons.h"
 #include "ConnectedComponents.h"
+#include "SeparateBranches.h"
 #include <set>
 
 /**
@@ -81,6 +82,88 @@ void EncodeEdge(const skeleton::GraphProjSkel::Ptr skel, const std::pair<unsigne
 	}
 }
 
+/**
+ *  \brief Asociate the first node to two sets representing an edge
+ */
+unsigned int AssociatedNode(const skeleton::GraphProjSkel::Ptr skel, const std::vector<unsigned int> &assocext, const std::set<unsigned int> &set_ext1, const std::set<unsigned int> &set_ext2)
+{
+	std::set<unsigned int> set_nod;
+	for(std::set<unsigned int>::const_iterator it = set_ext1.begin(); it != set_ext1.end(); it++)
+	{
+		set_nod.insert(assocext[*it]);
+	}
+	
+	unsigned int opp_nod = assocext[*(set_ext2.begin())];
+	
+	std::list<std::vector<unsigned int> > paths = algorithm::graphoperation::GetNodes(skel,*(set_nod.begin()),opp_nod);
+	
+	std::vector<unsigned int> &path = *(paths.begin());
+	
+	std::map<unsigned int, unsigned int> used;
+	
+	for(unsigned int i = 0; i < path.size(); i++)
+		used[path[i]] = 1;
+	
+	for(std::set<unsigned int>::iterator it = std::next(set_nod.begin()); it != set_nod.end(); it++)
+	{
+		std::list<std::vector<unsigned int> > paths2 = algorithm::graphoperation::GetNodes(skel,*(set_nod.begin()),opp_nod);
+		std::vector<unsigned int> &path2 = *(paths2.begin());
+		
+		for(unsigned int i = 0; i < path2.size(); i++)
+		{
+			std::map<unsigned int, unsigned int>::iterator itu = used.find(path2[i]);
+			if(itu!=used.end())
+				itu->second++;
+		}
+	}
+	
+	unsigned nbusedmax = used[path[path.size()-1]];
+
+	unsigned int indexnode = 0;
+
+	while(used[path[indexnode]] != nbusedmax && indexnode != path.size()-1)
+		indexnode++;
+
+	return indexnode;
+}
+
+/**
+ *  \brief Decodes an edge, from the extremities its separates
+ */
+void DecodeEdge(const skeleton::GraphProjSkel::Ptr skel, const std::vector<unsigned int> &assocext, const std::set<std::set<unsigned int> > &set_edg, std::pair<unsigned int, unsigned int> &edge)
+{
+	edge.first  = AssociatedNode(skel,assocext,(*set_edg.begin()),(*set_edg.rbegin()));
+	edge.second = AssociatedNode(skel,assocext,(*set_edg.rbegin()),(*set_edg.begin()));
+}
+
+/**
+ *  \brief Contracts an edge, replacing it by the middle node
+ */
+void ContractEdge(skeleton::GraphProjSkel::Ptr skelsimp, const skeleton::GraphProjSkel::Ptr skelori, const std::pair<unsigned int,unsigned int> &edge)
+{
+	std::list<std::vector<unsigned int> > listbr = algorithm::graphoperation::GetNodes(skelori, edge.first, edge.second);
+	std::vector<unsigned int> &br = *(listbr.begin());
+	
+	unsigned int node = br[br.size()/2];
+
+	std::list<unsigned int> neigh;
+	skelsimp->getNeighbors(edge.first,neigh);
+	skelsimp->getNeighbors(edge.second,neigh);
+
+	skelsimp->remNode(edge.first);
+	skelsimp->remNode(edge.second);
+
+	skelsimp->addNode(node,skelori->getNode(node));
+	
+	for(std::list<unsigned int>::iterator it = neigh.begin(); it != neigh.end(); it++)
+	{
+		if(*it != edge.first && *it != edge.second)
+		{
+			skelsimp->addEdge(node,*it);
+		}
+	}
+}
+
 skeleton::ReconstructionSkeleton::Ptr algorithm::graphoperation::TopoMatch(const std::vector<skeleton::GraphProjSkel::Ptr> &vec_skel, const std::vector<std::vector<unsigned int> > &assoc_ext)
 {
 	std::vector<skeleton::GraphProjSkel::Ptr> vec_skelsimp(vec_skel.size());
@@ -91,7 +174,7 @@ skeleton::ReconstructionSkeleton::Ptr algorithm::graphoperation::TopoMatch(const
 	for(unsigned int i = 0; i < vec_skel.size(); i++)
 	{
 		vec_skelsimp[i] = SimplifySkeleton(vec_skel[i]);
-
+		
 		std::list<std::pair<unsigned int,unsigned int> > listedges;
 		vec_skelsimp[i]->getAllEdges(listedges);
 		for(std::list<std::pair<unsigned int,unsigned int> >::iterator it = listedges.begin(); it != listedges.end(); it++)
@@ -110,6 +193,41 @@ skeleton::ReconstructionSkeleton::Ptr algorithm::graphoperation::TopoMatch(const
 		}
 	}
 	
+	// contracts edges which occurences is less than half the number of skeletons
+	for(unsigned int i = 0; i < vec_skelsimp.size(); i++)
+	{
+		std::vector<std::pair<unsigned int,unsigned int> > vecedges(0);
+		vec_skelsimp[i]->getAllEdges(vecedges);
+		std::vector<std::set<std::set<unsigned int> > > vecedgset(vecedges.size());
+
+		for(unsigned int j = 0; j < vecedges.size() ; j++)
+		{
+			EncodeEdge(vec_skelsimp[i],vecedges[j],assoc_ext[i],vecedgset[j]);
+		}
+		
+		for(std::map<std::set<std::set<unsigned int> >,unsigned int>::iterator it = cptedg.begin(); it != cptedg.end(); it++)
+		{
+			//if the edge is not kept, it is contracted
+			if(it->second <= vec_skel.size()/2)
+			{
+				unsigned int ind = std::find(vecedgset.begin(),vecedgset.end(),it->first) - vecedgset.begin();
+				if(ind != vecedgset.size())
+				{
+					ContractEdge(vec_skelsimp[i],vec_skel[i],vecedges[ind]);
+					
+					vecedges.resize(0);
+					vec_skelsimp[i]->getAllEdges(vecedges);
+					vecedgset.resize(vecedges.size());
+
+					for(unsigned int j = 0; j < vecedges.size() ; j++)
+					{
+						EncodeEdge(vec_skelsimp[i],vecedges[j],assoc_ext[i],vecedgset[j]);
+					}
+				}
+			}
+		}
+	}
+
 	std::list<std::set<std::set<unsigned int> > > keptedg;
 
 	// get most present edges
