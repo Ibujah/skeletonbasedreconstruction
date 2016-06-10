85043547dc9a4386d1617eda6708381c618c6b39
DURIX Bastien <bastien.durix@enseeiht.fr>
2016-06-10 22:38:03 +0200

    Added images and cameras saving in soft_acquisitions

--- src/soft/soft_acquisitions/main.cpp
+++ src/soft/soft_acquisitions/main.cpp
@@ -27,9 +27,12 @@ SOFTWARE.
  */
 
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 #include <camera/Camera.h>
 #include <tracking/Tracker.h>
@@ -42,14 +45,27 @@ SOFTWARE.
 int main(int argc, char** argv)
 {
 	std::string camfile;
+	std::string fold;
 	unsigned int camID;
+	unsigned int nbmarkers;
 
 	boost::program_options::options_description desc("OPTIONS");
+	time_t tim = time(0);
+	struct tm *now = std::localtime(&tim);
+	
+	std::ostringstream ossdate;
+	ossdate << "./acq" << std::setfill('0') << std::setw(2) << now->tm_year+1900 << "_" 
+					   << std::setfill('0') << std::setw(2) << now->tm_mon+1 << "_" 
+					   << std::setfill('0') << std::setw(2) << now->tm_mday << "_" 
+					   << std::setfill('0') << std::setw(2) << now->tm_hour << "_" 
+					   << std::setfill('0') << std::setw(2) << now->tm_min << "/";
 
 	desc.add_options()
 		("help", "Help message")
 		("camfile", boost::program_options::value<std::string>(&camfile)->default_value("cam.xml"), "Camera file")
 		("camID", boost::program_options::value<unsigned int>(&camID)->default_value(1), "Camera number")
+		("fold", boost::program_options::value<std::string>(&fold)->default_value(ossdate.str()), "Folder to save acquisitions in")
+		("nbmarkers", boost::program_options::value<unsigned int>(&nbmarkers)->default_value(6), "Number of markers to detect")
 		;
 
 	boost::program_options::variables_map vm;
@@ -60,6 +76,11 @@ int main(int argc, char** argv)
 		std::cout << desc << std::endl;
 		return 0;
 	}
+
+	boost::filesystem::path dir(fold);
+	if(!boost::filesystem::create_directory(dir))
+		return -1;
+	
 
 	camera::Camera::Ptr cam = fileio::ReadCamera(camfile);
 	tracking::Tracker tracker(4,0.4);
@@ -121,6 +142,7 @@ int main(int argc, char** argv)
 	disclass.setIntrinsics(cam->getIntrinsics());
 	
 	fini = false;
+	unsigned int nimg = 1;
 	while(!fini)
 	{
         cv::Mat view;
@@ -155,6 +177,18 @@ int main(int argc, char** argv)
 				frame_cur = mathtools::affine::Frame<3>::CreateFrame(matTr.col(3),matTr.col(0),matTr.col(1),matTr.col(2));
 				camera::Extrinsics::Ptr extr(new camera::Extrinsics(frame_cur->getFrameInverse()));
 				camera::Camera::Ptr camimg(new camera::Camera(cam->getIntrinsics(),extr));
+
+				std::ostringstream imgfname;
+				imgfname << fold << "img" << nimg << ".jpg";
+				std::ostringstream camfname;
+				camfname << fold << "cam" << nimg << ".xml";
+				cv::imwrite(imgfname.str(),aff);
+				fileio::WriteCamera(camimg,camfname.str());
+
+				std::cout << "Saved image at " << imgfname.str() << std::endl;
+				std::cout << "Saved camera at " << camfname.str() << std::endl;
+				nimg++;
+
 				display3d::DisplayCamera(disclass,camimg);
 			}
 			if(key=='q' || key=='Q')
